f1124c6e375abf4bd157ffa5cf9a17b8cfe97134
manikandan <mani2k3@rediffmail.com>
2018-05-19 12:37:18 +0530

    section page UI layout changes

--- src/main/webapp/app/section/section.component.ts
+++ src/main/webapp/app/section/section.component.ts
@@ -9,14 +9,26 @@ import { Http, Response, Headers, URLSearchParams, RequestOptions } from '@angul
 })
 export class SectionComponent implements OnInit {
   sectionId: number;
-  headerHeight;footerHeight;
-  segCont; greenOptCont; historical: any;
+  headerHeight; footerHeight;
+  segCont; greenOptCont; historical; diffZone; overCondition: any;
+  oneWidthfeet: number;
+  oneHeightFeet: number;
+  adjustedScreenWidth: number;
+  adjustedScreenHeight: number;
+  layoutWidthMargin: number;
+  layoutHeightMargin: number;
+  layoutWidth: number;
+  layoutHeight: number;
 
   constructor(private activatedRoute: ActivatedRoute, private http: Http) { }
 
   ngOnInit() {
+    this.layoutHeightMargin = 25;
     this.headerHeight = document.getElementById('header').offsetHeight;
     this.footerHeight = document.getElementById('footer').offsetHeight;
+    this.layoutWidth = window.innerWidth;
+    this.layoutHeight = window.innerHeight;
+    console.log('layoutheight'+ this.layoutHeight);
     this.activatedRoute.params.subscribe(params => {
       this.sectionId = params['sectionId'];
       console.log(this.sectionId);
@@ -25,11 +37,13 @@ export class SectionComponent implements OnInit {
     this.segmentCondition();
     this.greensOptimal();
     this.historicalKPI();
+    this.diffZones();
+    this.overallCondition();
   }
 
   segmentCondition() {
     return this.http.get('../assets/json/Wireframe2_c_Segment_Current_Condition_Top_Right_Corner.json').subscribe(
-      res => {        
+      res => {
         this.segCont = res.json();
         console.log(this.segCont);
       },
@@ -41,7 +55,7 @@ export class SectionComponent implements OnInit {
 
   greensOptimal() {
     return this.http.get('../assets/json/Wireframe2_b_Section_Optimal_Condition_Bottom_Right_Corner.json').subscribe(
-      res => {        
+      res => {
         this.greenOptCont = res.json();
         console.log(this.greenOptCont);
       },
@@ -53,7 +67,7 @@ export class SectionComponent implements OnInit {
 
   historicalKPI() {
     return this.http.get('../assets/json/Wireframe2_f_Historical_KPI_From_Sensor_Bottom_Left_Corner.json').subscribe(
-      res => {        
+      res => {
         this.historical = res.json();
         console.log(this.historical);
       },
@@ -62,4 +76,57 @@ export class SectionComponent implements OnInit {
       }
     );
   }
+
+  diffZones() {
+    return this.http.get('../assets/json/Wireframe2_e_Diff_Zones_with_Sensor_Middle_Compartment.json').subscribe(
+      res => {
+        this.diffZone = res.json();
+        console.log(this.diffZone);
+      },
+      err => {
+        console.log("Error occured.")
+      }
+    );
+  }
+
+  overallCondition() {
+    return this.http.get('../assets/json/Wireframe2_a_Section_Overall_Condition_Top_Left_Corner.json').subscribe(
+      res => {
+        this.overCondition = res.json();
+        console.log(this.overCondition);
+        this.prepareLayoutData(window.innerWidth, window.innerHeight);
+      },
+      err => {
+        console.log("Error occured.")
+      }
+    );
+  }
+
+  prepareLayoutData(screenWidth, screenHeight) {
+
+    this.adjustedScreenHeight = screenHeight - (this.headerHeight + this.footerHeight + (2 * this.layoutHeightMargin));
+    this.adjustedScreenWidth = this.getAdjustedScreenResolutionWidth(screenWidth, this.adjustedScreenHeight);
+    console.log(this.adjustedScreenHeight);
+    console.log(this.adjustedScreenWidth);
+
+    if (this.overCondition !== undefined) {
+
+      this.oneWidthfeet = (this.adjustedScreenWidth / this.overCondition.startX);
+      this.oneHeightFeet = (this.adjustedScreenHeight / this.overCondition.startY);
+      this.layoutWidthMargin = (screenWidth - this.adjustedScreenWidth) / 2;
+      console.log(this.oneWidthfeet);
+      console.log(this.oneHeightFeet);
+      console.log(this.layoutWidthMargin);
+    }
+
+  }
+
+  getAdjustedScreenResolutionWidth(screenWidth, screenHeight) {
+    //Aspect ratio based adjustment to get screen width
+    if (screenHeight * (this.overCondition.startX / this.overCondition.startY) > this.overCondition.startX)
+      return screenWidth * (this.overCondition.startY / this.overCondition.startX);
+    else
+      return screenHeight * (this.overCondition.startX / this.overCondition.startY);
+  }
+
 }
