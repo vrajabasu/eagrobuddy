d77923faa2183d92a0eb2e8aed2b60c89fbe7097
manikandan <mani2k3@rediffmail.com>
2018-06-03 20:32:39 +0530

    segment pattern and background color changes

--- src/main/webapp/app/dashboard/dashboard.component.ts
+++ src/main/webapp/app/dashboard/dashboard.component.ts
@@ -117,11 +117,11 @@ export class DashboardComponent implements OnInit {
   assignBgClr(clrvalue) {
     // Set back ground color based on overall threshold status
     if (clrvalue === 'EXCEEDED') {
-      return "#ff3333"; //#ff765e"; // Light RED
+      return "radial-gradient(white 5%, red 75%)"; //#ff765e"; // Light RED
     } else if (clrvalue === 'EXCEEDING_SOON') {
-      return '#ffff00' // Light Yellow
+      return "radial-gradient(white 5%, yellow 75%)"; // Light Yellow
     } else {
-      return '#00ff00' // Light Green
+      return "radial-gradient(white 5%, green 75%)"; // Light Green
     }
   }
 
@@ -140,11 +140,11 @@ export class DashboardComponent implements OnInit {
   assignBgClr2(clrvalue) {
     // Set back ground color based on overall threshold status
     if (clrvalue === 'EXCEEDED') {
-      return "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAYUlEQVQoU2P85e39nwEJhNTcYljTooYsBGYzIisEKYIBdMWkK0Q2DZupcBOJUohNEbqpYBORFZpPesFwMk8Cw1OMfsdVUYIHXSFIBygEUHwNEuzmO8RQ+skOfzhiyCIJAAA6zDihIDPubAAAAABJRU5ErkJggg==) repeat"; // Light Red
+      return "linear-gradient(#c21500, #ffc500)"; // Light Red
     } else if (clrvalue === 'EXCEEDING_SOON') {
-      return "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAYUlEQVQoU2P89EvoPwMSiDkrzLDE+C2yEJjNiKwQpAgG0BWTrhDZNGymwk0kSiE2Reimgk1EVmg+6QXDyTwJDE8x+h1XRQkedIUgHaAQQPE1SHBy/G+G3IWs+MMRQxZJAAC5xTztYN1JbwAAAABJRU5ErkJggg==) repeat"; // Light Yellow
+      return "linear-gradient(#d53369, #cbad6d)"; // Light Yellow
     } else {
-      return "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAYklEQVQoU2MUzlP9z4AErCMZGI4uRxaBsBmRFYIUwQC6YtIVIpuGzVS4iUQpxKYI3VSwicgKzSe9YDiZJ4HhKUa/46jBg64QpAMUAii+BgkWvX7B0CeKMBFmNIZCzKCGiAAAI1spfbSN7hEAAAAASUVORK5CYII=) repeat"; // Light Green
+      return "linear-gradient(#a73737, #7a2828)"; // Light Green
     } 
   }  
 
@@ -152,11 +152,11 @@ export class DashboardComponent implements OnInit {
   assignBgClr3(clrvalue) {
     // Set back ground color based on overall threshold status
     if (clrvalue === 'EXCEEDED') {
-      return "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAASklEQVQoU42QUQ4AIAhC4f6HtumykbllX5ZPSGiAESD28buX9e0A3tShKnApqUodDLAqqXX22EH533QIZgyOrTWW7zId/MQzDXwB0fg0A/d01CAAAAAASUVORK5CYII=) repeat"; // Light Red
+      return "linear-gradient(#f857a6, #ff5858)"; // Light Red
     } else if (clrvalue === 'EXCEEDING_SOON') {
-      return "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAS0lEQVQoU2P89UvjPxvbDUYGKADxQUx0MbgCkCSyJnQDUExCNgVdI1ghuknIVsPkGLEpgrkXZgNIDfEKibYaOVgIegabYozgITbAAZkDZHfNqOryAAAAAElFTkSuQmCC) repeat"; // Light Yellow
+      return "linear-gradient(#4b6cb7, #182848)"; // Light Yellow
     } else {
-      return "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAS0lEQVQoU2N0+aXxfw/bDUYGKADxQUx0MbgCkCSyJnQDUExCNgVdI1ghuknIVsPkGLEpgrkXZgNIDfEKibYaOVgIegabYozgITbAASXZYZ/hJtSoAAAAAElFTkSuQmCC) repeat"; // Light Green
+      return "linear-gradient(#e43a15, #e65245)"; // Light Green
     } 
   }    
 
@@ -165,11 +165,11 @@ export class DashboardComponent implements OnInit {
   assignBgClr4(clrvalue) {
     // Set back ground color based on overall threshold status
     if (clrvalue === 'EXCEEDED') {
-      return "url("+"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 80 40' width='80' height='40'%3E%3Cpath fill='%232069bb' fill-opacity='0.4' d='M0 40a19.96 19.96 0 0 1 5.9-14.11 20.17 20.17 0 0 1 19.44-5.2A20 20 0 0 1 20.2 40H0zM65.32.75A20.02 20.02 0 0 1 40.8 25.26 20.02 20.02 0 0 1 65.32.76zM.07 0h20.1l-.08.07A20.02 20.02 0 0 1 .75 5.25 20.08 20.08 0 0 1 .07 0zm1.94 40h2.53l4.26-4.24v-9.78A17.96 17.96 0 0 0 2 40zm5.38 0h9.8a17.98 17.98 0 0 0 6.67-16.42L7.4 40zm3.43-15.42v9.17l11.62-11.59c-3.97-.5-8.08.3-11.62 2.42zm32.86-.78A18 18 0 0 0 63.85 3.63L43.68 23.8zm7.2-19.17v9.15L62.43 2.22c-3.96-.5-8.05.3-11.57 2.4zm-3.49 2.72c-4.1 4.1-5.81 9.69-5.13 15.03l6.61-6.6V6.02c-.51.41-1 .85-1.48 1.33zM17.18 0H7.42L3.64 3.78A18 18 0 0 0 17.18 0zM2.08 0c-.01.8.04 1.58.14 2.37L4.59 0H2.07z'%3E%3C/path%3E%3C/svg%3E"+")"; // Light Red
+      return "linear-gradient(#C04848, #480048)"; // Light Red
     } else if (clrvalue === 'EXCEEDING_SOON') {
-      return "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAS0lEQVQoU2P89UvjPxvbDUYGKADxQUx0MbgCkCSyJnQDUExCNgVdI1ghuknIVsPkGLEpgrkXZgNIDfEKibYaOVgIegabYozgITbAAZkDZHfNqOryAAAAAElFTkSuQmCC) repeat"; // Light Yellow
+      return "linear-gradient(#5f2c82, #49a09d)"; // Light Yellow
     } else {
-      return "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAS0lEQVQoU2N0+aXxfw/bDUYGKADxQUx0MbgCkCSyJnQDUExCNgVdI1ghuknIVsPkGLEpgrkXZgNIDfEKibYaOVgIegabYozgITbAASXZYZ/hJtSoAAAAAElFTkSuQmCC) repeat"; // Light Green
+      return "linear-gradient(#24C6DC, #514A9D)"; // Light Green
     } 
   }  
 
