446a25294f16113c95334cdc35cedd05a0810f39
Rajee <rajeshwari39@gmail.com>
2018-06-24 22:29:02 +0530

    spring security stateless jwt role based authentication

---
+++ src/main/java/com/eagro/security/SecurityConstants.java
@@ -0,0 +1,22 @@
+package com.eagro.security;
+
+/**
+ * JHipster constants.
+ */
+public interface SecurityConstants {
+
+    // Spring profiles for development, test and production, see http://www.jhipster.tech/profiles/
+    String SPRING_PROFILE_DEVELOPMENT = "dev";
+    String SPRING_PROFILE_TEST = "test";
+    String SPRING_PROFILE_PRODUCTION = "prod";
+    // Spring profile used when deploying with Spring Cloud (used when deploying to CloudFoundry)
+    String SPRING_PROFILE_CLOUD = "cloud";
+    // Spring profile used when deploying to Heroku
+    String SPRING_PROFILE_HEROKU = "heroku";
+    // Spring profile used to disable swagger
+    String SPRING_PROFILE_SWAGGER = "swagger";
+    // Spring profile used to disable running liquibase
+    String SPRING_PROFILE_NO_LIQUIBASE = "no-liquibase";
+    // Spring profile used when deploying to Kubernetes and OpenShift
+    String SPRING_PROFILE_K8S = "k8s";
+}
